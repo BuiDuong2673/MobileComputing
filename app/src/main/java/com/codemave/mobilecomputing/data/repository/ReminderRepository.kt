8ad24089d0bb5e7b07963597568f3f497d78a9b0
Bui Thi Bach Duong <bachduong26723@gmail.com>
2023-03-06 13:09:47 +0200

    The 3rd version of the app

---
+++ app/src/main/java/com/codemave/mobilecomputing/data/repository/ReminderRepository.kt
@@ -0,0 +1,36 @@
+package com.codemave.mobilecomputing.data.repository
+
+import com.codemave.mobilecomputing.data.entity.Reminder
+import com.codemave.mobilecomputing.data.room.ReminderDao
+import com.codemave.mobilecomputing.data.room.ReminderToCategory
+import kotlinx.coroutines.flow.Flow
+
+class ReminderRepository (
+    private val reminderDao: ReminderDao
+) {
+    /**
+     * return a flow containing the list of reminders associated with the category
+     * with the given [categoryId]
+     */
+    fun remindersInCategory(categoryId: Long): Flow<List<ReminderToCategory>> {
+        return reminderDao.reminderFromCategory(categoryId)
+    }
+    /**
+     * Get reminder with the given reminderId
+     */
+    fun getReminderWithId(reminderId: Long): Reminder? {
+        return reminderDao.reminder(reminderId)
+    }
+    /**
+     * Add a new [Reminder] to the reminder store
+     */
+    suspend fun addReminder(reminder: Reminder) = reminderDao.insert(reminder)
+    /**
+     * Delete a [Reminder] from the reminder store
+     */
+    suspend fun deleteReminder(reminder: Reminder) = reminderDao.delete(reminder)
+    /**
+     * Edit a [Reminder] in the reminder store
+     */
+    suspend fun editReminder(reminder: Reminder) = reminderDao.update(reminder)
+}
